65e3e4c1db49383591efcacb83366e226d68a0bf
wodby <wodby@example.com>
2023-08-10 20:54:05 +0000

    Actualizo resources

--- web/modules/custom/resources/js/resources.js
+++ web/modules/custom/resources/js/resources.js
@@ -4,5 +4,10 @@
     console.log('resourcesUpdateData is called.');
     // Set textfield's value to the passed arguments.
     $('input#edit-title-0-value').attr('value', arguments['title']);
+    $('input#edit-field-url-img-resources-0-uri').attr('value', arguments['small_image_id']);
+    $('input#edit-field-url-img-resources-h-0-uri').attr('value', arguments['big_image_id']);
+    $('img#small_image').attr('src', arguments['small_image_id']);
+    $('img#big_image').attr('src', arguments['big_image_id']);
+    $('input#edit-field-url-resources-0-uri').attr('value', arguments['viewer_url']);
   };
 })(jQuery);
