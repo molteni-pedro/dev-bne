1542601874c4fd4e88fd85031241e189191e74b6
Diego Gutierrez <diegosgutierrez@outlook.com>
2023-08-11 00:59:07 -0300

    Agrego funcionalidad para tomar descripción y fechas - TODO: rellenar campo ckeditor con datos (están en el textarea pero no se ve renderizado en ckeditor)

--- web/modules/custom/resources/js/resources.js
+++ web/modules/custom/resources/js/resources.js
@@ -3,11 +3,14 @@
   $.fn.resourcesUpdateData = function(arguments) {
     console.log('resourcesUpdateData is called.');
     // Set textfield's value to the passed arguments.
-    $('input#edit-title-0-value').attr('value', arguments['title']);
+    $('input#edit-title-0-value').val(arguments['title']);
     $('input#edit-field-url-img-resources-0-uri').attr('value', arguments['small_image_id']);
     $('input#edit-field-url-img-resources-h-0-uri').attr('value', arguments['big_image_id']);
     $('img#small_image').attr('src', arguments['small_image_id']);
     $('img#big_image').attr('src', arguments['big_image_id']);
     $('input#edit-field-url-resources-0-uri').attr('value', arguments['viewer_url']);
+    $('input#edit-field-physical-description-0-value').attr('value', arguments['physical_description']);
+    $('input#edit-field-publication-1-year-0-value').attr('value', arguments['date_1']);
+    $('input#edit-field-publication-2-year-0-value').attr('value', arguments['date_2']);
   };
 })(jQuery);
