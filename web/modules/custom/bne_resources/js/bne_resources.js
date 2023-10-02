5353576112902ea32bd0ab0bd37e6b3d27cc6224
Molteni Pedro <moltenipedro@gmail.com>
2023-10-02 18:11:13 -0300

    update module

--- web/modules/custom/bne_resources/js/bne_resources.js
+++ web/modules/custom/bne_resources/js/bne_resources.js
@@ -1,35 +1,11 @@
 (function ($) {
-  Drupal.behaviors.bneResources = {
+  Drupal.behaviors.myCustomCHSBehavior = {
     attach: function (context, settings) {
-      // Detectar cambios en el campo field_subject_resources.
-      $('#edit-field-subject-resources', context).once('bneResources').on('change', function () {
-        // Obtener los valores seleccionados de los términos (TID) de las taxonomías.
-        var selectedTIDs = [];
-        $('#edit-field-subject-resources option:selected').each(function () {
-          if ($(this).val() !== '_none') {
-            selectedTIDs.push($(this).val());
-          }
-        });
-
-        // Construir la URL utilizando Drupal.url() para la solicitud AJAX.
-        var ajaxUrl = Drupal.url('bne_resources/ajax/update_taxonomy/' + selectedTIDs.join(','));
-
-        // Mostrar un mensaje de depuración con los TIDs seleccionados.
-        console.log('TIDs seleccionados: ' + selectedTIDs.join(', ')); // Usar console.log en lugar de alert para depuración.
-
-        // Realizar una petición AJAX.
-        if (selectedTIDs.length > 0) {
-          // Crear una nueva instancia de Drupal.ajax.
-          var ajaxSettings = {
-            url: ajaxUrl,
-            // Ajusta las opciones de la solicitud según tus necesidades.
-          };
-          var ajaxObject = new Drupal.ajax(false, $('#edit-field-subject-resources', context)[0], ajaxSettings);
-          ajaxObject.eventResponse($(this), context, ajaxSettings);
-        }
-
-        // Mostrar los TID seleccionados en el campo de "markup."
-        $('#custom-selected-terms', context).html(selectedTIDs.join(', '));
+      // Escucha cambios en el campo CHS con el evento 'cschierarchyupdate'.
+      $('#field-resource-level', context).once('my-custom-chs-behavior').on('cschierarchyupdate', function () {
+        // Tu código JavaScript personalizado aquí.
+        // Por ejemplo, muestra un mensaje de alerta.
+        alert('Seleccionaste un valor en el campo CHS');
       });
     }
   };
